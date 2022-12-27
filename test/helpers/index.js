457024de337a3fe6b1c7ceec0251850fb94b0454
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-27 16:06:44 -0300

    add readFile tests

---
+++ test/helpers/index.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const fs = require('fs')
+const fsp = require('fs/promises')
+const os = require('os')
+
+const isWin = os.platform() === 'win32'
+
+module.exports = {
+  createTmpDir,
+  createFolder,
+  isWin
+}
+
+function createTmpDir (t) {
+  const tmpdir = path.join(os.tmpdir(), 'localdrive-test-')
+  const dir = fs.mkdtempSync(tmpdir)
+  t.teardown(() => fsp.rm(dir, { recursive: true }))
+  return dir
+}
+
+function createFolder (t, { setup = true } = {}) {
+  const root = createTmpDir(t)
+  if (setup) generateTestFiles(t, root)
+
+  return root
+}
+
+function generateTestFiles (t, root) {
+  const fullpath = (name) => path.join(root, name)
+  const createFile = (name, content) => fs.writeFileSync(fullpath(name), content)
+  const createFolder = (name) => fs.mkdirSync(fullpath(name))
+
+  createFile('README.md', '# example')
+  createFile('script.sh', '#!/bin/bash')
+  createFile('LICENSE', 'MIT')
+  createFile('key.secret', '1234')
+  createFile('empty.txt', '')
+
+  createFolder('examples/')
+  createFile('examples/a.txt', '1st')
+  createFile('examples/b.txt', '2th')
+
+  createFolder('examples/more/')
+  createFile('examples/more/c.txt', '3rd')
+  createFile('examples/more/d.txt', '4th')
+
+  createFolder('solo/')
+  createFile('solo/one.txt', '5th')
+
+  fs.chmodSync(fullpath('key.secret'), 0o222)
+  fs.chmodSync(fullpath('script.sh'), 0o755)
+  if (!isWin) fs.symlinkSync('LICENSE', fullpath('LICENSE.shortcut'))
+}
---
+++ test/read-file.js
@@ -0,0 +1,109 @@
+const test = require('brittle')
+const path = require('path')
+const { createFolder } = require('./helpers')
+const fs = require('../index.js')
+const b4a = require('b4a')
+
+test('read file', function (t) {
+  t.plan(1)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'LICENSE'), function (err, data) {
+    if (err) throw err
+    t.alike(data, b4a.from('MIT'))
+  })
+})
+
+test('read file with encoding', function (t) {
+  t.plan(2)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'LICENSE'), 'utf-8', function (err, data) {
+    if (err) throw err
+    t.is(data, 'MIT')
+  })
+
+  fs.readFile(path.join(root, 'LICENSE'), 'utf8', function (err, data) {
+    if (err) throw err
+    t.is(data, 'MIT')
+  })
+})
+
+test('read non-existing file', function (t) {
+  t.plan(3)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'not-exists.txt'), function (err, data) {
+    t.is(err.errno, -2)
+    t.is(err.code, 'ENOENT')
+    t.is(data, undefined)
+  })
+})
+
+test('read file with encoding option', function (t) {
+  t.plan(1)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'LICENSE'), { encoding: 'utf-8' }, function (err, data) {
+    if (err) throw err
+    t.is(data, 'MIT')
+  })
+})
+
+test.skip('read file with non-existing encoding', function (t) {
+  t.plan(2)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'LICENSE'), { encoding: 'not-exists' }, function (err, data) {
+    t.is(err.code, 'ERR_UNKNOWN_ENCODING')
+    t.is(data, undefined)
+  })
+})
+
+test('read file but it is a folder', function (t) {
+  t.plan(3)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'examples'), { encoding: 'utf-8' }, function (err, data) {
+    // + it should be:
+    // Error: EISDIR: illegal operation on a directory
+
+    t.is(err.errno, -21)
+    t.is(err.code, 'EUNKNOWN')
+    t.is(data, undefined)
+  })
+})
+
+test('read file with flags', function (t) {
+  t.plan(1)
+
+  const root = createFolder(t)
+
+  fs.readFile(path.join(root, 'LICENSE'), { flag: 'w+' }, function (err, data) {
+    if (err) throw err
+    t.alike(data, b4a.from('')) // empty means that w+ flags had effect
+  })
+})
+
+/*
+exports.open = open
+exports.close = close
+exports.unlink = unlink
+exports.read = read
+exports.readFile = readFile
+exports.write = write
+exports.writeFile = writeFile
+exports.mkdir = mkdir
+exports.rmdir = rmdir
+exports.Stats = Stats // for compat
+exports.stat = stat
+exports.fstat = fstat
+exports.lstat = lstat
+exports.ftruncate = ftruncate
+*/

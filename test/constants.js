b96755c51fe37d30a742d69cbd1ef8632694a814
Lucas Barrena <lucasbarrena98@gmail.com>
2023-02-01 13:24:52 -0300

    fix windows tests

--- test/constants.js
+++ test/constants.js
@@ -3,7 +3,14 @@ const fsnode = require('fs')
 const fs = require('../index.js')
 
 test('constants', function (t) {
+  const windowsOnly = ['S_IFBLK', 'S_IFIFO', 'S_IFSOCK']
+
   for (const key in fs.constants) {
+    if (windowsOnly.indexOf(key) > -1) {
+      t.is(fs.constants[key], 0, key + ' is exported (win32 only)')
+      continue
+    }
+
     t.is(fs.constants[key], fsnode.constants[key], key + ' is exported')
   }
 
